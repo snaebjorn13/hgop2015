0525300f59ffe063161f1362bdcc1ac0cf57c9d1
snaebjorn13 <snabbie1@gmail.com>
2015-12-03 22:35:42 +0000

    moved join game test into another file

--- server/model/tictactoe/whenCreateGame.spec.js
+++ server/model/tictactoe/whenCreateGame.spec.js
@@ -27,37 +27,3 @@ describe('create game command', function () {
 			.exactly(JSON.stringify(then));
 	});
 });
-
-describe('join game command', function () {
-	var given, when, then;
-
-	it ('should join game', () => {
-		given = [{
-			id:        '1110',
-			comm:      'GameCreated',
-			userName:  'gunnar',
-			name:      'AnotherGame',
-			timeStamp: '2015.12.03T15:32:29'
-		}];
-		when = {
-			id:            '1111',
-			comm:          'JoinGame',
-			userName:      'daniel',
-			otherUserName: 'gunnar',
-			timeStamp:     '2015.12.03T15:34:10'
-		};
-		then = [{
-			id:            '1111',
-			event:         'GameJoined',
-			userName:      'daniel',
-			otherUserName: 'gunnar',
-			timeStamp:     '2015.12.03T15:34:10'
-		}];
-
-		var actualEvents = ticTacToeCommandHandler(given)
-							.executeCommand(when);
-
-		JSON.stringify(actualEvents).should.be
-			.exactly(JSON.stringify(then));
-	});
-});
