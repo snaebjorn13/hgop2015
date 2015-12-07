41cdfd2f31d40c9539c3b8588f6f0d25867bbfa5
snaebjorn13 <snabbie1@gmail.com>
2015-12-07 17:06:28 +0000

    added missing code from gulli

---
+++ server/api/joinGame/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var express = require('express');
+
+module.exports = function(eventStore){
+
+	var controller = require('../command.controller')(eventStore);
+
+	var router = express.Router();
+	router.post('/', controller.executeCommand);
+
+	return {
+		router:router
+	}
+};
