8635cdca0ef6704a2ac4a38831abce48392a5613
snaebjorn13 <snabbie1@gmail.com>
2015-12-14 13:43:36 +0000

    appended port number to docker container names to distinguish between test and prod containers

--- deploymentscript.sh
+++ deploymentscript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker kill tictactoe
-docker rm tictactoe
+docker kill tictactoe$1
+docker rm tictactoe$1
 docker pull snaebjorn13/tictactoe:$GIT_PREVIOUS_SUCCESSFUL_COMMIT
-docker run --name tictactoe -p $1:8080 -d -e "NODE_ENV=production" snaebjorn13/tictactoe:$GIT_PREVIOUS_SUCCESSFUL_COMMIT
+docker run --name tictactoe$1 -p $1:8080 -d -e "NODE_ENV=production" snaebjorn13/tictactoe:$GIT_PREVIOUS_SUCCESSFUL_COMMIT
