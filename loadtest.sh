f12bc520e9da0bd7876b65e563361b016a613096
snaebjorn13 <snabbie1@gmail.com>
2015-12-14 11:09:20 +0000

    added fixes to prevent output when in test mode

--- loadtest.sh
+++ loadtest.sh
@@ -2,4 +2,6 @@
 echo Restarting docker image..
 ssh vagrant@192.168.33.10 'docker restart tictactoe'
 export ACCEPTANCE_URL=http://192.168.33.10:9000
+export MOCHA_REPORTER=xunit
+export MOCHA_REPORT=test-reports/mocha/server-tests.xml
 grunt mochaTest:load
--- server/config/express.js
+++ server/config/express.js
@@ -26,12 +26,11 @@ module.exports = function(app) {
   app.use(bodyParser.json());
   app.use(methodOverride());
   app.use(cookieParser());
-  
+
   if ('production' === env) {
     app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
     app.use(express.static(path.join(config.root, 'public')));
     app.set('appPath', config.root + '/public');
-    app.use(morgan('dev'));
   }
 
   if ('development' === env || 'test' === env) {
@@ -39,7 +38,10 @@ module.exports = function(app) {
     app.use(express.static(path.join(config.root, '.tmp')));
     app.use(express.static(path.join(config.root, 'client')));
     app.set('appPath', 'client');
-    app.use(morgan('dev'));
     app.use(errorHandler()); // Error handler - has to be last
+  }
+
+  if ('test' !== env) {
+    app.use(morgan('dev'));
   }
 };
