d843bbfe0688a8732b36e77b2c49bb571ea3091e
snaebjorn13 <snabbie1@gmail.com>
2015-12-14 15:38:38 +0000

    whoops made a mistake in deploymentscript

--- deploymentscript.sh
+++ deploymentscript.sh
@@ -4,5 +4,5 @@ echo Updating to $2, running on port $1
 
 docker kill tictactoe$1
 docker rm tictactoe$1
-docker pull snaebjorn13/tictactoe:$GIT_PREVIOUS_SUCCESSFUL_COMMIT
-docker run --name tictactoe$1 -p $1:8080 -d -e "NODE_ENV=production" snaebjorn13/tictactoe:$GIT_PREVIOUS_SUCCESSFUL_COMMIT
+docker pull snaebjorn13/tictactoe:$2
+docker run --name tictactoe$1 -p $1:8080 -d -e "NODE_ENV=production" snaebjorn13/tictactoe:$2
