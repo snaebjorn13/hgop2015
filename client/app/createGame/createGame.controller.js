528801aebf6374fc8b5d098dee47b30d6b69cbf2
snaebjorn13 <snabbie1@gmail.com>
2015-12-15 14:34:14 +0000

    added files for client, modified CreateGame to my API

---
+++ client/app/createGame/createGame.controller.js
@@ -0,0 +1,26 @@
+'use strict';
+
+angular.module('tictactoeApp')
+  .controller('CreateGameCtrl', function ($scope, $http, guid, $location) {
+    $scope.createGame = function () {
+      var user = {'userName': $scope.userName, side: 'X'};
+
+      var id = guid();
+      var gameId = guid();
+      var createPost = $http.post('/api/createGame/', {
+          'id': id,
+          'gameId': gameId,
+          'comm': 'CreateGame',
+          'userName': user,
+          'timeStamp': '2014-12-02T11:29:29'
+        }
+      );
+      createPost.then(function (response) {
+        $location.url('/tictactoe');
+        $location.search('gameId', response.data[0].gameId);
+        $location.search('gameSide', 'X');
+      });
+
+    };
+
+  });
